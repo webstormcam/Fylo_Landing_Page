c2b12846f9ff536b24c6fccfbfbedd2d0b6d6cf5
Cameron Chandler <webstormcam@gmail.com>
2021-10-01 11:11:31 -0400

    Made seperate forms

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function validateForm(){
      }
     document.getElementById('error').innerHTML=text;
 }
+
+
+function validateForm2(){
+    let x=document.getElementById('email2').value;
+    let text;
+    if (x ===''){
+        text=' Oops! Please add your email'
+    
+    } else if (x.includes('@')==false){
+        text='Oops! Please check your email'
+    
+    }else if (x.length<5 && x.includes('@')===true){
+        text='Oops! Please check your email'
+   
+    } else if (x.length>=6 && x.includes('@')===true){
+        text=''
+        document.getElementById("myForm2").submit();
+     }
+    document.getElementById('error2').innerHTML=text;
+}
